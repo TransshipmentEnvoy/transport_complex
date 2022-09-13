18278864c9db64f6ff4c8c968388623f1d9726d9
TransshipmentEnvoy <TransshipmentEnvoy@outlook.com>
2022-09-13 23:23:23 +0800

    clean develop build

--- script/clean_setup.sh
+++ script/clean_setup.sh
@@ -3,3 +3,4 @@ rm src/tcomplex/libspdlog.so.1.10.0
 rm src/tcomplex/libspdlog.so.1
 rm src/tcomplex/_if.cpython-39-x86_64-linux-gnu.so
 rm src/tcomplex/libnanobind.so
+rm -r src/tcomplex/libtcomplex
