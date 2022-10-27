0e6b05800cab8fd51a5044b97a4bae72f79defa3
TransshipmentEnvoy <TransshipmentEnvoy@outlook.com>
2022-10-28 00:24:55 +0800

    [script] update path

--- script/clean_setup.sh
+++ script/clean_setup.sh
@@ -1,6 +1,9 @@
-rm src/tcomplex/libtcomplex.so
-rm src/tcomplex/libspdlog.so.1.10.0
-rm src/tcomplex/libspdlog.so.1
-rm src/tcomplex/_if.cpython-39-x86_64-linux-gnu.so
-rm src/tcomplex/libnanobind.so
-rm -r src/tcomplex/libtcomplex
+rm src/tcomplex/ext/libtcomplex.so
+# rm src/tcomplex/ext/libspdlog.so.1.10.0
+# rm src/tcomplex/ext/libspdlog.so.1
+rm src/tcomplex/ext/libspdlog.so
+rm src/tcomplex/ext/libnanobind.so
+rm -r src/tcomplex/ext/libtcomplex
+
+rm src/tcomplex/_if.cpython-310-x86_64-linux-gnu.so
+rm src/tcomplex/util/upkeep/log.cpython-310-x86_64-linux-gnu.so
